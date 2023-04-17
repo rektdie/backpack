75b5856fd4343af65425bba0bcaabd8a6f425e24
rektdie <rektdie@gmail.com>
2023-04-17 22:17:09 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+API_KEY = "1BE7CD78BC0E81FB9FF4B35A5D070429"
+
+steamIDs = [
+    76561198323251063, # Enyém
+    76561199013264816 # Rolié
+]
+
+gameID = 730
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    return text
+
+response = requests.get(f"http://steamcommunity.com/inventory/{steamIDs[1]}/{gameID}/2")
+
+with open("data.txt", "w") as f:
+    f.write(jprint(response.json()))
