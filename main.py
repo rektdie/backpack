22b2c2cba50286859cd0174eb1b50392c50c0512
rektdie <rektdie@gmail.com>
2023-04-18 00:11:59 +0200

    Testing version

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import requests
 import json
 
-API_KEY = "1BE7CD78BC0E81FB9FF4B35A5D070429"
+API_KEY = "0K3XC6K5VRGZ5XFI"
 
 steamIDs = [
     76561198323251063, # Enyém
     76561199013264816 # Rolié
 ]
 
-gameID = 730
+gameID = "csgo"
 
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     return text
 
-response = requests.get(f"http://steamcommunity.com/inventory/{steamIDs[1]}/{gameID}/2")
+parameters = {
+    "steam_id": steamIDs[1],
+    "game": gameID,
+    "language": "english",
+    "parse": 0,
+}
+
+response = requests.get(f"https://steamwebapi.com/steam/api/inventory?key={API_KEY}", params=parameters)
 
 with open("data.txt", "w") as f:
     f.write(jprint(response.json()))
+
+print(response.status_code)
