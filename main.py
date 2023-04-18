74822db44815ae257abbe572d3e9c47165244e1c
rektdie <115177215+rektdie@users.noreply.github.com>
2023-04-18 23:41:43 +0200

    Tried with delaying

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 import json
+import time
 
 API_KEY = "1BE7CD78BC0E81FB9FF4B35A5D070429"
 
 steamIDs = [
-    76561198323251063, # Enyém
-    76561199013264816 # Rolié
+    76561198323251063, # Én
+    76561199013264816, # Roli
+    76561199067585535 # Peti
 ]
 
 gameID = "730"
@@ -14,8 +16,11 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     return text
 
-response = requests.get(f"http://steamcommunity.com/inventory/{steamIDs[1]}/{gameID}/2")
-print(response.status_code)
+for steamID in steamIDs:
+    response = requests.get(f"http://steamcommunity.com/inventory/{steamID}/{gameID}/2")
+    
+    print(response.status_code)
 
-with open("data.txt", "w") as f:
-    f.write(jprint(response.json()))
+    with open("data.txt", "a") as f:
+        f.write(jprint(response.json()) + "\n")
+    time.sleep(1)
