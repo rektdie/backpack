434e1df280567560bc333a52241e4111d40b9d41
rektdie <115177215+rektdie@users.noreply.github.com>
2023-04-18 13:31:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import requests
 import json
 
-API_KEY = "0K3XC6K5VRGZ5XFI"
+API_KEY = "1BE7CD78BC0E81FB9FF4B35A5D070429"
 
 steamIDs = [
     76561198323251063, # Enyém
     76561199013264816 # Rolié
 ]
 
-gameID = "csgo"
+gameID = "730"
+
+with open("valid_proxies.txt", "r") as f:
+    proxies = f.read().split("\n")
 
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     return text
 
-parameters = {
-    "steam_id": steamIDs[1],
-    "game": gameID,
-    "language": "english",
-    "parse": 0,
-}
 
-response = requests.get(f"https://steamwebapi.com/steam/api/inventory?key={API_KEY}", params=parameters)
+for proxy in proxies:
+    try:
+        response = requests.get(f"http://steamcommunity.com/inventory/{steamIDs[0]}/{gameID}/2", proxies={"http": proxy, "https": proxy})
+        print(proxy)
+    except:
+        continue
+
+    if response.status_code == 200:
+        print(response.status_code)
+
 
 with open("data.txt", "w") as f:
     f.write(jprint(response.json()))
-
-print(response.status_code)
