ab9e693195e52c19f1e58a45f85f7cd34c473779
rektdie <115177215+rektdie@users.noreply.github.com>
2023-04-18 22:37:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,23 +10,12 @@ steamIDs = [
 
 gameID = "730"
 
-with open("valid_proxies.txt", "r") as f:
-    proxies = f.read().split("\n")
-
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     return text
 
+response = requests.get(f"http://steamcommunity.com/inventory/{steamIDs[1]}/{gameID}/2")
+print(response.status_code)
 
-for proxy in proxies:
-    try:
-        response = requests.get(f"http://steamcommunity.com/inventory/{steamIDs[0]}/{gameID}/2", proxies={"http": proxy, "https": proxy})
-        print(proxy)
-    except:
-        continue
-
-    if response.status_code == 200:
-        print(response.status_code)
-
-    with open("data.txt", "w") as f:
-        f.write(jprint(response.json()))
+with open("data.txt", "w") as f:
+    f.write(jprint(response.json()))
