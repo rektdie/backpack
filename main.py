6889821f6416a960e0fe277b9740ae10b8be8166
rektdie <rektdie@gmail.com>
2023-04-20 21:25:23 +0200

    Module problems still occur

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import requests
 import json
+from a2s import A2SInfo, A2SPlayer
 
-API_KEY = "0K3XC6K5VRGZ5XFI"
-
-steamIDs = [
-    76561198323251063, # Enyém
-    76561199013264816 # Rolié
-]
-
-gameID = "csgo"
+API_KEY = "953d9838-b77a-4823-a99c-44e9de511138"
+gameID = "440"
 
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     return text
 
-parameters = {
-    "steam_id": steamIDs[1],
-    "game": gameID,
-    "language": "english",
-    "parse": 0,
-}
+session = requests.Session()
 
-response = requests.get(f"https://steamwebapi.com/steam/api/inventory?key={API_KEY}", params=parameters)
+# Get server address from the game client
+server_address = None
+with A2SInfo("hl2master.steampowered.com", 27011) as server:
+    try:
+        for info in server.filter(gamedir="tf"):
+            if info.server_name == "Team Fortress":
+                server_address = (info.server_address, info.server_port)
+                break
+        if not server_address:
+            print("No TF2 server found.")
+            exit(1)
+    except:
+        print("Master server query failed.")
+        exit(1)
 
-with open("data.txt", "w") as f:
-    f.write(jprint(response.json()))
+# Get list of players on the server
+with A2SPlayer(server_address) as server:
+    players = server.get_players()
+steamIDs = [str(player.steamid) for player in players]
 
-print(response.status_code)
+# Get inventory for each player
+for steamID in steamIDs:
+    response = session.get(f"https://hexa.one/api/v1/user/inventory/{steamID}/{gameID}/2", headers={"X-API-Key": API_KEY})
+
+    with open("data.txt", "a") as f:
+        f.write(jprint(response.json()) + "\n")
