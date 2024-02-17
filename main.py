c56945586889258cbb1cbdb3eb5bc353770d3707
rektdie@gmail.com <rektdie@gmail.com>
2024-02-17 18:08:07 +0100

    added database to avoid double checking

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 from steamid_converter import Converter
 import re
 
-API_KEY = "953d9838-b77a-4823-a99c-44e9de511138"
+API_KEY = "ea6b6952-e49d-43f4-887b-3a99da0fc452"
 
 my_steam_id = "76561198323251063"
 
@@ -30,22 +30,27 @@ session = requests.Session()
 
 for steamID in steamIDs:
     if steamID != my_steam_id:
-        try:
-            response = session.get(f"https://hexa.one/api/v1/user/inventory/{steamID}/{gameID}/2", headers={"X-API-Key": API_KEY}, timeout=10)
-            data = json.loads(response.text)
-            print(response.elapsed.total_seconds())
+        with open("database.txt", "a+") as f:
+            if (steamID not in f):
+                f.write(steamID + '\n')
+                try:
+                    response = session.get(f"https://hexa.one/api/v1/user/inventory/{steamID}/{gameID}/2", headers={"X-API-Key": API_KEY}, timeout=10)
+                    data = json.loads(response.text)
+                    print(response.elapsed.total_seconds())
 
-            num_cases = 0
-            try:
-                for item in data['result']['inventory'].values():
-                    if 'CSGO_Type_WeaponCase' in [tag['internal_name'] for tag in item['tags']]:
-                        num_cases += item['amount']
-            except:
-                continue
+                    num_cases = 0
+                    try:
+                        for item in data['result']['inventory'].values():
+                            if 'CSGO_Type_WeaponCase' in [tag['internal_name'] for tag in item['tags']]:
+                                num_cases += item['amount']
+                    except:
+                        continue
 
-            if num_cases > 0:
-                with open("results.txt", "a") as f:
-                        f.write(f"{num_cases} db: http://steamcommunity.com/profiles/{steamID}\n")
-        except requests.exceptions.Timeout:
-            print("Timed out")
-            continue
+                    if num_cases > 0:
+                        with open("results.txt", "a") as f:
+                                f.write(f"{num_cases} db: http://steamcommunity.com/profiles/{steamID}\n")
+                except requests.exceptions.Timeout:
+                    print("Timed out")
+                    continue
+            else:
+                print(f"{steamID} already in database")
